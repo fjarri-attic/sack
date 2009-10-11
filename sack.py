6c84c8c40d801e026cc209bd745ae9b72820b5de
Bogdan Opanchuk <bg@bk.ru>
2009-10-11 18:32:32 +1100

    sack: moved main menu to a separate class

--- sack.py
+++ sack.py
@@ -8,9 +8,16 @@ main = DBWindow()
 
 main.show()
 
-menubar = QtGui.QMenuBar()
-file = menubar.addMenu('&File')
-edit = menubar.addMenu('&Edit')
-tools = menubar.addMenu('&Tools')
+class MainMenu(QtGui.QMenuBar):
+
+	def __init__(self):
+		QtGui.QMenuBar.__init__(self)
+
+		file = self.addMenu('&File')
+		edit = self.addMenu('&Edit')
+		view = self.addMenu('&View')
+		tools = self.addMenu('&Tools')
+
+main_menu = MainMenu()
 
 sys.exit(app.exec_())
