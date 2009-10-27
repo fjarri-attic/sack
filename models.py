02ac2351fe636fa5bc6a27112b18f37f27faeb90
Bogdan Opanchuk <bg@bk.ru>
2009-10-28 00:53:27 +1100

    models: added root and built-in classes to DB model
    + added getTitle() function and title patterns

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from PyQt4 import QtCore, QtGui
 
+from logging import warning
+import re
+
 import brain
+import brain.op as op
 
 import parser
 
@@ -14,18 +18,139 @@ class DatabaseCache:
 		return getattr(self._conn, name)
 
 
+class Template:
+
+	delimiter = '$'
+	idpattern = r'[_a-z][_a-z0-9\.\-]*'
+
+	pattern = r"""
+	%(delim)s(?:
+		(?P<escaped>%(delim)s) |   # Escape sequence of two delimiters
+		(?P<named>%(id)s)      |   # delimiter and a Python identifier
+		{(?P<braced>%(id)s)}   |   # delimiter and a braced identifier
+		(?P<invalid>)              # Other ill-formed delimiter exprs
+	)
+	"""
+
+	pattern = pattern % {
+		'delim' : re.escape(delimiter),
+		'id' : idpattern,
+	}
+
+	pattern = re.compile(pattern, re.IGNORECASE | re.VERBOSE)
+
+	def __init__(self, template):
+		self._template = template
+
+	def getFieldNames(self):
+
+		names = set()
+
+		def convert(mo):
+			named = mo.group('named') or mo.group('braced')
+			if named is not None:
+				names.add(named)
+
+		self.pattern.sub(convert, self._template)
+		return names
+
+	def substitute(self, field_values):
+
+		def convert(mo):
+			named = mo.group('named')
+			if named is not None:
+				if named in field_values:
+					return str(field_values[named])
+				else:
+					return self.delimiter + named
+
+			braced = mo.group('braced')
+			if braced is not None:
+				if braced in field_values:
+					return str(field_values[braced])
+				else:
+					return self.delimiter + '{' + braced + '}'
+
+			if mo.group('escaped') is not None:
+				return self.delimiter
+
+			return self.delimiter + mo.group('invalid')
+
+		return self.pattern.sub(convert, self._template)
+
+
 class DatabaseModel(QtCore.QObject):
 
 	def __init__(self, file_name, new_file):
 		QtCore.QObject.__init__(self)
 		self._db = DatabaseCache(file_name, new_file)
 
+		self._findRoot()
+
+	def _findRoot(self):
+
+		# Search for root object - it is the starting point for
+		# accessing the database
+		result = self._db.search(['_Class'], op.EQ, None)
+
+		if result == []:
+
+			self._class_class = self._db.create({
+				'Name': 'Metaclass',
+				'TitleTemplate': '$Name',
+				'FieldsOrder': ['Name', 'TitleTemplate', 'FieldsOrder']
+			})
+			self._db.modify(self._class_class, ['_Class'], self._class_class)
+
+			self._tag_class = self._db.create({
+				'Name': 'Tag class',
+				'TitleTemplate': '$Name',
+				'_Class': self._class_class,
+				'FieldsOrder': ['Name', 'Description']
+			})
+
+			self._default_class = self._db.create({
+				'Name': 'Default class',
+				'TitleTemplate': '$Title',
+				'_Class': self._class_class,
+				'FieldsOrder': ['Title', 'Data']
+			})
+
+			self._root = self._db.create({
+				'_Tags': [],
+				'_Class': None,
+				'BuiltinClasses': {
+					'Class': self._class_class,
+					'Tag': self._tag_class,
+					'Default': self._default_class
+				}
+			})
+		else:
+			if len(result) > 1:
+				warning("More than one root object found (" +
+					repr(result) + "), using the first one")
+			self._root = result[0]
+
+			self._class_class = self._db.read(self._root, ['BuiltinClasses', 'Class'])
+			self._tag_class = self._db.read(self._root, ['BuiltinClasses', 'Tag'])
+			self._default_class = self._db.read(self._root, ['BuiltinClasses', 'Default'])
+
 	def getTitle(self, id):
-		title = self._db.read(id, ['_Title'])
+		obj_class = self._db.read(id, ['_Class'])
+		title_template = self._db.read(obj_class, ['TitleTemplate'])
+		template_obj = Template(title_template)
 
-		# TODO: add expandable titles (like "%(Track)-%(Name)")
+		field_names = template_obj.getFieldNames()
+		field_values = {}
+		for name in field_names:
+			try:
+				value = self._db.read(id, name.split('.'))
+			except brain.LogicError:
+				pass
 
-		return title
+			field_values[name] = value
+
+		return template_obj.substitute(field_values)
 
 	def __getattr__(self, name):
 		return getattr(self._db, name)
