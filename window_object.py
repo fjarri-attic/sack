d26cc74613c66f224e3dc0576c4756bb8b2890e2
Bogdan Opanchuk <bg@bk.ru>
2009-12-03 21:56:30 +1100

    window_object: added ObjectModel stub

--- window_object.py
+++ window_object.py
@@ -5,14 +5,33 @@ import brain
 from globals import *
 
 
+class ObjectModel(QtCore.QAbstractItemModel):
+
+	def __init__(self, db_model, obj_id):
+		QtCore.QAbstractItemModel.__init__(self)
+		self._db_model = db_model
+		self._obj_id = obj_id
+
+
+class ObjectView(QtGui.QTreeView):
+
+	def __init__(self, db_model, obj_id):
+		QtGui.QTreeView.__init__(self)
+		self.setModel(ObjectModel(db_model, obj_id))
+
+
 @dynamically_translated
-class ObjectWindow(QtGui.QLabel):
+class ObjectWindow(QtGui.QSplitter):
 
 	titleChanged = QtCore.pyqtSignal(str)
 
 	def __init__(self, parent, db_model, obj_wrapper):
-		QtGui.QLabel.__init__(self, parent)
-		self.setText(db_model.getTitle(obj_wrapper.id))
+		QtGui.QSplitter.__init__(self, QtCore.Qt.Vertical, parent)
+		self._db_model = db_model
+		self._obj_id = obj_wrapper.id
+
+		obj_view = ObjectView(self._db_model, self._obj_id)
+		self.addWidget(obj_view)
 
 	def title(self):
-		return "Object View"
+		return self._db_model.getTitle(self._obj_id)
