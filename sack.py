da9652f1851d44cd2df940ee905492e6627e0ba8
Bogdan Opanchuk <bg@bk.ru>
2010-01-12 17:53:55 +1100

    sack: fixed log init function according to changes in qthelpers

--- sack.py
+++ sack.py
@@ -2,7 +2,7 @@ import sys
 import qthelpers
 import application
 
-qthelpers.log.init()
+qthelpers.initLog()
 qthelpers.importResources()
 app = application.Application(sys.argv)
 sys.exit(app.exec_())
